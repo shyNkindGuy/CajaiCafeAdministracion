1abb9151fce172becf1033349d6d091464a18e2c
piero alarcon <leonardoalarcon751@gmail.com>
2025-10-25 15:03:37 -0500

    feat: funcion para conteo de commits de gradle para automatizar la version de la aplicacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,28 @@
+import java.util.concurrent.TimeUnit;
+import org.gradle.api.GradleException;
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
 }
+
+//funcion para obtener conteno de commits
+
+fun getGitcommit(): Int{
+    return try{
+        val process = ProcessBuilder("git", "rev-list", "--count","HEAD")
+            .redirectErrorStream(true)
+            .start()
+        process.waitFor(5, TimeUnit.SECONDS)
+        val output = process.inputStream.bufferedReader().readText();
+
+        val count = output.trim().toInt()
+        if (count < 1) 1 else count
+    }catch (e: Exception){
+        1
+    }
+}
+
+val commitCount = getGitcommit()
 
 android {
     namespace = "com.example.cajaicafeadministracion"
@@ -11,8 +32,8 @@ android {
         applicationId = "com.example.cajaicafeadministracion"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = commitCount
+        versionName = "1.0.$commitCount"//se usa la varible para automatizarlo
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
